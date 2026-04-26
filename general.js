051dc3f4d623e9559b9644ebdb720d3b90e606d2
ABUBAKAR IBRAHIM <ibrahimabubakar741@gmail.com>
2026-04-26 12:24:37 +0100

    Update general.js

--- general.js
+++ general.js
@@ -4,7 +4,7 @@ const axios = require('axios');
 public_users.get('/', async function (req, res) {
   try {
     const response = await axios.get("http://localhost:5000/");
-    res.send(JSON.stringify(response.data, null, 4));
+    res.status(200).json(response.data);
   } catch (error) {
     res.status(500).json({message: "Error fetching books"});
   }
@@ -18,15 +18,18 @@ public_users.get('/isbn/:isbn', function (req, res) {
     .catch(err => res.status(404).json({message: "ISBN not found"}));
 });
 
-// Task 12: Get book details based on Author
-public_users.get('/author/:author', function (req, res) {
+// Task 12: Get book details based on Author using Async/Await
+public_users.get('/author/:author', async function (req, res) {
   const author = req.params.author;
-  axios.get(`http://localhost:5000/author/${author}`)
-    .then(response => res.status(200).json(response.data))
-    .catch(err => res.status(404).json({message: "Author not found"}));
+  try {
+    const response = await axios.get(`http://localhost:5000/author/${author}`);
+    res.status(200).json(response.data);
+  } catch (error) {
+    res.status(404).json({message: "Author not found"});
+  }
 });
 
-// Task 13: Get book details based on Title
+// Task 13: Get book details based on Title using Promises
 public_users.get('/title/:title', function (req, res) {
   const title = req.params.title;
   axios.get(`http://localhost:5000/title/${title}`)
